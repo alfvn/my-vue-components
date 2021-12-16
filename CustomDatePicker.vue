0ca7cd0126100229ae96ebe99ab05c1917584126
yanglei302 <yanglei302@jd.com>
2021-12-16 09:32:09 +0800

    feat: customDatePicker

---
+++ CustomDatePicker.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-date-picker
+    v-model="value"
+    v-bind="$attrs"
+    @change="handleChange"
+    v-on="$listeners"
+    >
+  </el-date-picker>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    begin:{
+      type: [String, Number, Object, Date],
+      default: ''
+    },
+    end:{
+      type: [String, Number, Object, Date],
+      default: ''
+    },
+  },
+  data(){
+    return {
+      value: []
+    }
+  },
+  methods: {
+    handleChange(){
+      this.$emit('update:begin', this.value && this.value[0])
+      this.$emit('update:end', this.value && this.value[1])
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ demo/useDatePicker.vue
@@ -0,0 +1,26 @@
+<template>
+  <custom-date-picker
+    :begin.sync="effectStartTime"
+    :end.sync="effectEndTime"
+    value-format="timestamp"
+    type="datetimerange"
+    range-separator="至"
+    start-placeholder="开始日期"
+    end-placeholder="结束日期"
+    @change="handleChange"/>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      effectStartTime: '',
+      effectEndTime: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
